fc6f62c1dc10d6ce37e238ab566405bb0d3ac455
Sanket2412 <khardekarsanket@gmail.com>
2025-12-27 16:33:10 +0530

    [Added] Linkedlist version

---
+++ add-two-numbers/v2.js
@@ -0,0 +1,22 @@
+function ListNode(val, next = null) {
+  this.val = val;
+  this.next = next;
+}
+
+var addTwoNumbers= function(l1,l2){
+    let result= new ListNode(0),
+    carry=0,
+    current=result;
+    while(l1 !==null || l2 !==null || carry > 0)
+    {
+        const val1 = l1 ? l1.val : 0;
+        const val2 = l2 ? l2.val : 0;
+        const sum = val1 + val2 + carry;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+    return result;
+}
