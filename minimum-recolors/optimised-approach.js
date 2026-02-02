dccb24b39a7bfced33168e55a1ef9213de720f41
Sanket2412 <khardekarsanket@gmail.com>
2026-02-02 23:59:09 +0530

    [Added] optimised approach

---
+++ minimum-recolors/optimised-approach.js
@@ -0,0 +1,21 @@
+var minimumRecolors = function(blocks, k) {
+    let n = blocks.length;
+    let whiteCount = 0;
+    for (let i = 0; i < k; i++) {
+        if (blocks[i] === 'W') {
+            whiteCount++;
+        }
+    }
+    let minRecolors = whiteCount;
+    for (let i = k; i < n; i++) {
+        if (blocks[i] === 'W') {
+            whiteCount++;
+        }
+        if (blocks[i - k] === 'W') {
+            whiteCount--;
+        }
+        minRecolors = Math.min(minRecolors, whiteCount);
+    }
+
+    return minRecolors;
+};
